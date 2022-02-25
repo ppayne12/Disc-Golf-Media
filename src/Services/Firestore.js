01ef153cf0723ad3abe1e221a76bd2d4058c147a
ppayne12 <ppayne12@gmail.com>
2022-02-25 06:52:06 -0700

    remove API key

--- src/Services/Firestore.js
+++ src/Services/Firestore.js
@@ -11,21 +11,6 @@ import {
 } from "firebase/firestore/lite";
 import { getAuth, signInAnonymously } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCXcaB6KB9QRI56cD_FWLahk1nk1K0SGvo",
-//   authDomain: "discgolf-media.firebaseapp.com",
-//   projectId: "discgolf-media",
-//   storageBucket: "discgolf-media.appspot.com",
-//   messagingSenderId: "174408656803",
-//   appId: "1:174408656803:web:274b256aba3059411b1c16",
-//   measurementId: "G-8S5YSLYBRS",
-// };
-
 // Initialize Firebase
 const app = initializeApp(fs.firebaseConfig);
 const analytics = getAnalytics(app);
