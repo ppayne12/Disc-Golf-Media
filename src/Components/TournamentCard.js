04a070378bb0fefb485d6cab6e30381f04bf97ef
ppayne12 <ppayne12@gmail.com>
2022-03-20 09:59:37 -0700

    added channel

--- src/Components/TournamentCard.js
+++ src/Components/TournamentCard.js
@@ -17,6 +17,7 @@ import AceRunPro from "../images/acerunpro.png";
 import GKPro from "../images/gkpro.webp";
 import CHSports from "../images/chsport.jpeg";
 import DiscGolfGuy from "../images/discgolfguy.png";
+import DiscGolfNetwork from "../images/dgpt.jpeg";
 import * as FirestoreService from "../Services/Firestore";
 import PublicIcon from "@mui/icons-material/Public";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
@@ -40,6 +41,7 @@ export default function TournamentCard(props) {
     GKPro,
     CHSports,
     DiscGolfGuy,
+    DiscGolfNetwork,
   };
 
   const [expanded, setExpanded] = React.useState(false);
